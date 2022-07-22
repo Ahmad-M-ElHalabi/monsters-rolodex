92ca92d6d7e07514c20c67265cd175b87e9213d3
Ahmad-M-ElHalabi <41826672+Ahmad-M-ElHalabi@users.noreply.github.com>
2022-07-22 14:10:27 +0300

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-lest.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import"./App.css"
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters :[],
+      SearchFiled:""
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response=>Response.json())
+    .then(users=>this.setState({monsters:users}))
+  }
+  handleChange=(e) =>{
+    this.setState({SearchFiled:e.target.value})
+  }
+  render(){
+    const {monsters, SearchFiled} = this.state;
+    const filterdMonsters= monsters.filter(monster=>monster.name.toLowerCase().includes(SearchFiled.toLocaleLowerCase()))
+    return(
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="search monsters" handleChange={this.handleChange}/>
+        <CardList monsters={filterdMonsters}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./search-box.style.css"
+export const SearchBox = ({placeholder, handleChange}) =>(
+    <input className="search" placeholder={placeholder} type="search" onChange={handleChange}/>
+)
